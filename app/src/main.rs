68d230263557dd6d41efa3b1620ee7ee117210d4
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-01-06 15:38:12 -0500

    remove extra basic parts

--- app/src/main.rs
+++ app/src/main.rs
@@ -3,9 +3,6 @@ extern crate osw_lib;
 fn main() {
     println!("Hello, world!");
 
-    let return_value = osw_lib::add(1, 2);
-
-    osw_lib::some_other_test();
-
-    println!("The value is {}", return_value);
+    println!("calling basic function");
+    osw_lib::basic_dummy();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-pub fn some_other_test() {
-    println!("this is some other test files");
-    println!("random test lib");
-    println!("another random test lib");
+pub fn basic_dummy() -> () {
+    println!("basic dummy function for testing");
 }
